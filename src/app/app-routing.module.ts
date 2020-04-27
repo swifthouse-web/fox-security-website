b707a82abfa98c54b2bc6ce740986e76fb2af9b1
Heinrich Venter <heini141@outlook.com>
2020-04-27 15:43:36 +0200

    added navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: MainComponent
+  },
+  {
+    path: 'contact-us',
+    component: ContactUsComponent
+  },
+  {
+    path: '',
+    component: MainComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }];
+
+const config: ExtraOptions = {
+  useHash: false,
+};
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, config)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { MainComponent } from "./components/main/main.component";
+import { ContentComponent } from "./components/content/content.component";
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { RouterModule } from '@angular/router';
+
+const COMMON_COMPONENTS = [
+  MainComponent,
+  ContentComponent,
+  ContactUsComponent
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ...COMMON_COMPONENTS
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
