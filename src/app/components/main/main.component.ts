e9022d327f12fe8cd5dc79dcb973850143c104bd
Heinrich Venter <heini141@outlook.com>
2020-04-29 18:10:10 +0200

    updated api url

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ContentService } from 'src/app/services/content.service';
+import { Title, Meta } from '@angular/platform-browser';
+import { MarkdownService } from 'ngx-markdown';
+import { environment } from "../../../environments/environment";
 
 @Component({
   selector: 'app-main',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  pageName = 'home';
+  page: any;
+
+  constructor(
+    readonly router: Router,
+    readonly contentService: ContentService,
+    readonly titleService: Title,
+    readonly meta: Meta,
+    readonly markdownService: MarkdownService
+  ) { }
 
   ngOnInit() {
+    this.contentService.byPageName(this.pageName)
+      .subscribe(response => this.applyPage(response));
+  }
+
+  applyPage(page) {
+    page[0].page_content_primary = this.markdownService.compile(page[0].page_content_primary);
+    page[0].page_content_secondary = this.markdownService.compile(page[0].page_content_secondary);
+
+    this.page = page[0];
+
+    this.titleService.setTitle(`Fox Security - ${this.page.page_title}`);
+    this.meta.addTag(
+      {
+        name: 'description',
+        content: this.page.page_description
+      }
+    );
+  }
+
+  contentUrl(url) {
+    if (url)
+      return `${environment.cmsUrl}${url}`;
+
+    return '';
   }
 
 }
