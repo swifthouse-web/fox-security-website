d5b507a6e79e95db727943703f5964fcd8e6d6b1
Heinrich Venter <heini141@outlook.com>
2020-05-04 12:11:31 +0200

    added gradients for hero banners

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -47,7 +47,14 @@ export class ContentComponent implements OnInit {
 
     if (this.pageName === 'gallery') {
       this.contentService.getGalleries()
-        .subscribe((response: any[]) => this.gallery = response);
+        .subscribe((response: any[]) => {
+          this.gallery = response;
+          this.gallery.forEach(x => {
+            window.setTimeout(() => {
+              this.showSlides(this.concat(x.gallery_name), 1);
+            }, 11000);
+          });
+        });
     }
 
     if (this.pageName === 'downloads') {
@@ -64,4 +71,51 @@ export class ContentComponent implements OnInit {
     return '';
   }
 
+  slideIndex = 1;
+
+  // Next/previous controls
+  plusSlides(name, n) {
+    this.showSlides(name, this.slideIndex += n);
+  }
+
+  // Thumbnail image controls
+  currentSlide(name, n) {
+    this.showSlides(name, this.slideIndex = n);
+  }
+
+  showSlides(name, n) {
+    let i;
+    const slides = document.getElementsByClassName(name);
+    const dots = document.getElementsByClassName(`${name}-dot`);
+
+    if (n > slides.length) {
+      this.slideIndex = 1;
+    }
+
+    if (n < 1) {
+      this.slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+      if (!slides[i].className.match(/(d-none)/)) {
+        slides[i].className += ' d-none';
+      }
+
+    }
+
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(/(\sactive)/, '');
+    }
+
+    slides[this.slideIndex - 1].className.replace(/(\sd-none)/, '');
+    dots[this.slideIndex - 1].className += ' active';
+  }
+
+  concat(str: string) {
+    return str.replace(/(\s)/, '-');
+  }
+
+  count(arr: any[]) {
+    return arr.length;
+  }
 }
